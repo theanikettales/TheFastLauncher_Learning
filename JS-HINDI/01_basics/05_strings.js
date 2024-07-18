109505081de5d53232c9bcd5d8f6a33ce72a171b
theanikettales <theanikettales@gmail.com>
2024-07-18 07:55:13 +0530

    strings config done

--- JS-HINDI/01_basics/05_strings.js
+++ JS-HINDI/01_basics/05_strings.js
@@ -1,11 +1,11 @@
-const name = "aniket"
+const name = "hitesh"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('aniket-hc-com')
+const gameName = new String('hitesh-hc-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -22,11 +22,11 @@ console.log(newString);
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
 
-const newStringOne = "   aniket    "
+const newStringOne = "   hitesh    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
-const url = "https://aniket.com/aniket%20choudhary"
+const url = "https://hitesh.com/hitesh%20choudhary"
 
 console.log(url.replace('%20', '-'))
 
