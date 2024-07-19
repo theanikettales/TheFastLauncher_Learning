a00245684061832ce8e3957485e19e2533502b1c
theanikettales <theanikettales@gmail.com>
2024-07-19 12:44:06 +0530

    strings configure

--- JS-HINDI/01_basics/05_strings.js
+++ JS-HINDI/01_basics/05_strings.js
@@ -1,11 +1,11 @@
-const name = "hitesh"
+const name = "aniket"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('aniket-hc-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -22,7 +22,7 @@ console.log(newString);
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
 
-const newStringOne = "   hitesh    "
+const newStringOne = "   aniket    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
