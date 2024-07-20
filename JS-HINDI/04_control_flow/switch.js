057eea17ca4e1f0fba111eef2c6ca6e29c050e97
theanikettales <theanikettales@gmail.com>
2024-07-20 18:25:56 +0530

    control flow code files

---
+++ JS-HINDI/04_control_flow/switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "2"
+switch (month) {
+    case "1":
+        console.log("January");
+        break;
+    case "2":
+        console.log("feb");
+        break;
+    case "3":
+        console.log("march");
+        break;
+    case "4":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
