59a2e9576f604633bd653f4804050768c42f16a9
theanikettales <theanikettales@gmail.com>
2024-07-29 01:22:58 +0530

    DOM

---
+++ 5 Project with dom/project-5/script.js
@@ -0,0 +1,19 @@
+var arr = [
+{dp:&w=687&q=80", story:"https://images.unsplash.
+    com/photo-1509631179647-0177331693ae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&
+    fit=crop&w=688&q=80"},
+    {dp:"https://images.unsplash.com/photo-1604681630513-69474a4e253f?ixlib=rb-4.0.38 ixid=M3wxMjA3fDB8MHxwaG90by1wYWd1fHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=627&q=80", story:"https://images.unsplash.
+    com/photo-1604681527661-41ab3a47b2c4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&
+    fit=crop&w=704&q=80"},
+    {dp:"https://images.unsplash.com/photo-1604651403159-e42f29f5ea87?ixlib=rb-4.0.38
+    ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80", story:"https://images.unsplash. com/photo-1604436553922-b8e12ec7224d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWd1fHx8fGVufDB8fHx8fA%3D%3D&auto=format& fit=crop&w=687&q=80"},
+    {dp:"https://images.unsplash.com/photo-1613998479628-b4d8735b04f8?ixlib=rb-4.0.3&
+    ixid=M3wxMjA3fDB8MHxwaG90by1wYWd1fHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80", story:"https://images.unsplash. com/photo-1669607314566-0b3e589ec71d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&
+    fit=crop&w=687&q=80"},
+    I
+    1
+    var clutter = ""
+    arr.forEach(function(){
+    })
+    clutter += "Hello"
+    console.log(clutter)
