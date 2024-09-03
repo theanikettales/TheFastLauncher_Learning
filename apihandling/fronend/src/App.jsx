a805db28c336bd02c395c0d82c71aa71d91e9716
theanikettales <theanikettales@gmail.com>
2024-09-04 00:39:11 +0530

    reactjs

--- apihandling/fronend/src/App.jsx
+++ apihandling/fronend/src/App.jsx
@@ -1,45 +1,78 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import axios from 'axios'
+  import { useEffect, useState } from 'react'
+  import reactLogo from './assets/react.svg'
+  import viteLogo from '/vite.svg'
+  import './App.css'
+  import axios from 'axios'
 
-function App() {
+  function App() {
+
+  // const [products, error, loading] = customReactQuery('/api/products')
+
   const [products, setProducts] = useState([])
   const [error, setError] = useState(false)
   const [loading, setLoading] = useState(false)
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
-    (async () => {
-     try {
-       setError(false)
-       const response = await axios.get('/api/products')
-       console.log(response.data);
-       setProducts(response.data)
-       setLoading(false)
-     } catch (error) { 
-       setError(true)
-       setLoading(false)
-     }
+    const controller = new AbortController()
+    ;(async () => {
+      try {
+        setError(false)
+        const response = await axios.get('/api/products?search=' + search, {
+          signal: controller.signal
+        })
+        console.log(response.data);  
+        setProducts(response.data)
+        setLoading(false)
+      } catch (error) {
+        if (axios.isCancel(error)) {
+          log('Request canceled', error.message)
+          return
+        }
+        setError(true)
+        setLoading(false)
+      }
 
     })()
-    
-  }, [])
 
-  if (error) {
-    return<h1>Something went wrong</h1>
+    //cleaup
+    return() => {
+      controller.abort()
+    }
+  }, [search])
+
+
+
+  //   if (error) {
+    //   return<h1>Something went wrong</h1>
+    // }
+  // 
+    // if (loading) {
+    //   return <h1>Loading...</h1>
+    // }
+
+    return (
+      <>
+        <h1>Chai aur API in react</h1>
+        <input type="text" placeholder="Search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        
+        />
+
+
+        {loading && (<h1> Loading...</h1>)}
+        {error && (<h1>Something went wrong</h1>)}
+
+        <h2>Number of Products are : {products.length}</h2>
+      </>
+    )
   }
 
-  if (loading) {
-    return <h1>Loading...</h1>
+  export default App
+
+
+  const customReactQuery = (urlPath) => {
+
+    return [products, error, loading]
   }
-
-  return (
-    <>
-      <h1>Chai aur API in react</h1>
-      <h2>Number of Products are : {products.length}</h2>
-    </>
-  );
-}
-
-export default App
